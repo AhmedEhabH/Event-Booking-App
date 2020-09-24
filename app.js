5b7b43ba6660af9d8cc9e7880f3645bd5ea1f390
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-09-24 22:16:01 +0200

    Handle CORS application

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const CORS = require('cors');
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
@@ -10,6 +11,18 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(express.json());
+
+app.use(CORS());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(isAuth);
 
@@ -43,5 +56,5 @@ mongoose
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`you are listen to port ${PORT}`);  
+    console.log(`you are listen to port ${PORT}`);
 });
